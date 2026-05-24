adf03d4067a061022cbae62f41787c878eac6311
paka04sep <pakawat.20074@gmail.com>
2026-05-24 13:05:35 +0700

    test dev deploy vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="th" className={`${prompt.variable} h-full antialiased`}>
+    <html lang="th" className={`${prompt.variable} h-full antialiased`} suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
